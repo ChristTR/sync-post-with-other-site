4221a02d964fd12405190df7fc454f0648916d06
ChristTR <christr000@gmail.com>
2025-05-09 17:03:41 -0300

    Update sps_settings.class.php

--- includes/sps_settings.class.php
+++ includes/sps_settings.class.php
@@ -1,106 +1,183 @@
 <?php
-if( !class_exists ( 'SPS_Settings' ) ) {
+if (!class_exists('SPSv2_Settings')) {
 
-    class SPS_Settings {
+    class SPSv2_Settings {
 
-    	function __construct(){
-
-    		add_action( "sps_save_settings", array( $this, "sps_save_settings_func" ), 10 , 1 );
-
-    	}
-
-    	function sps_display_settings( ) {
-    		if( file_exists( SPS_INCLUDES_DIR . "sps_settings.view.php" ) ) {
-    			include_once( SPS_INCLUDES_DIR . "sps_settings.view.php" );
-    		}
-    	}
-
-        function sps_default_setting_option() {
-            return array(
-                'sps_host_name' => array( '0' => '' ),
-                'sps_strict_mode' => array( '0' => '1' ),
-                'sps_content_match' => array( '0' => 'title' ),
-                'sps_content_username' => array( '0' => '' ),
-                'sps_content_password' => array( '0' => '' ),
-                'sps_selected' => array( '0' => ''),
-                'sps_roles_allowed' => array( '0' => array('roles' => array( 'administrator' => 'on', 'editor' => 'on', 'author' => 'on' ) ) )
-            );
+        function __construct(){
+            add_action("spsv2_save_settings", array($this, "spsv2_save_settings"), 10, 1);
+            add_action('admin_init', array($this, 'spsv2_register_settings'));
         }
 
-    	function sps_save_settings_func( $params = array() ) {
-            $nonce = wp_create_nonce('sps_nonce');
-            if ( ! isset( $_POST['sps_general_option_field'] ) || ! wp_verify_nonce( $_POST['sps_general_option_field'], 'sps_nonce' ) ) {
-                // Nonce verification failed; handle error or exit.
-                wp_die('verification failed. Please try again');
+        // ============ [ NOVOS RECURSOS ] ============ //
+        
+        public function spsv2_register_settings() {
+            register_setting('spsv2_settings_group', 'spsv2_settings', array(
+                'sanitize_callback' => array($this, 'spsv2_sanitize_settings')
+            ));
+        }
+
+        public function spsv2_display_settings() {
+            global $spsv2;
+            ?>
+            <div class="wrap spsv2-settings-wrapper">
+                <h1><?php _e('Configurações Sync Post v2', 'spsv2_text_domain'); ?></h1>
+                
+                <?php if(isset($_SESSION['spsv2_msg'])) : ?>
+                    <div class="notice notice-<?php echo $_SESSION['spsv2_msg_type']; ?>">
+                        <p><?php echo $_SESSION['spsv2_msg']; ?></p>
+                    </div>
+                    <?php unset($_SESSION['spsv2_msg']); ?>
+                <?php endif; ?>
+
+                <form method="post" action="options.php">
+                    <?php 
+                    settings_fields('spsv2_settings_group');
+                    $settings = $this->spsv2_get_settings();
+                    $hosts = $settings['hosts'] ?? array();
+                    ?>
+                    
+                    <div id="spsv2-hosts-container">
+                        <?php foreach($hosts as $index => $host) : ?>
+                            <div class="spsv2-host-card">
+                                <?php $this->spsv2_render_host_fields($index, $host); ?>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+
+                    <button type="button" class="button" id="spsv2-add-host">
+                        <?php _e('+ Adicionar Novo Site', 'spsv2_text_domain'); ?>
+                    </button>
+
+                    <?php submit_button(__('Salvar Configurações', 'spsv2_text_domain')); ?>
+                </form>
+            </div>
+            <?php
+        }
+
+        // ============ [ MÉTODOS PRINCIPAIS ] ============ //
+
+        public function spsv2_save_settings($params) {
+            if (!current_user_can('manage_options')) {
+                SPSv2_Logger::log("Tentativa de acesso não autorizado às configurações", 'security');
+                wp_die(__('Acesso negado!', 'spsv2_text_domain'));
             }
 
-    		if( isset( $params['sps_setting'] ) && $params['sps_setting'] != '') {
-                $sps_setting = $params['sps_setting'];
-                unset( $params['sps_setting'] );
-    			unset( $params['sps_setting_save'] );
-
-                if( isset($params['sps_host_name']) && !empty($params['sps_host_name']) ) {
-                    $hostnames = array();
-                    $usernames = array();
-                    $passwords = array();
-                    foreach ($params['sps_host_name'] as $key => $hostname) {
-                        $hostnames[] = sanitize_url($hostname);
-                        $usernames[] = sanitize_user($params['sps_content_username'][$key]);
-                        $passwords[] = wp_strip_all_tags($params['sps_content_password'][$key]);
-                    }
-
-                    $params['sps_host_name'] = $hostnames;
-                    $params['sps_content_username'] = $usernames;
-                    $params['sps_content_password'] = $passwords;
-                }
-
-                update_option('sps_setting', $params);
-
-    			$_SESSION['sps_msg_status'] = true;
-    			$_SESSION['sps_msg'] = 'Settings updated successfully.';
-
-    		}
-    	}
-
-        function sps_get_settings_func( ) {
-            $ncm_default_general_option = $this->sps_default_setting_option();
-            $ncm_setting_option = get_option( 'sps_setting' );
-            return shortcode_atts( $ncm_default_general_option, $ncm_setting_option );
-        }
-
-        function sps_get_admin_users( $all_user = false, $fields = 'ID' ) {
-            global $wpdb;
-            $args = array(
-                'role'         => 'Administrator',
-                'orderby'      => 'ID',
-                'order'        => 'ASC',
-                'fields'       => $fields 
-            );
-            $users = get_users( $args );
-            if( $all_user ) {
-                return $users;
+            if (wp_verify_nonce($_POST['_wpnonce'], 'spsv2_settings_group-options')) {
+                $settings = $this->spsv2_sanitize_settings($_POST['spsv2_settings']);
+                update_option('spsv2_settings', $settings);
+                
+                $_SESSION['spsv2_msg'] = __('Configurações salvas com sucesso!', 'spsv2_text_domain');
+                $_SESSION['spsv2_msg_type'] = 'success';
+                
+                SPSv2_Logger::log("Configurações atualizadas", 'info');
             } else {
-                return $users[0];
+                $_SESSION['spsv2_msg'] = __('Falha na verificação de segurança!', 'spsv2_text_domain');
+                $_SESSION['spsv2_msg_type'] = 'error';
             }
         }
 
-        function sps_get_post_types() {
-            $all_types = get_post_types();
-            $unset_keys = array( 'attachment', 'revision', 'nav_menu_item', 'custom_css', 'customize_changeset', 'oembed_cache', 'user_request', 'wp_block' );
-
-            foreach ($unset_keys as $uvalue) {
-                if( isset($all_types[$uvalue]) ) {
-                    unset($all_types[$uvalue]);
+        private function spsv2_sanitize_settings($input) {
+            $output = array();
+            
+            if(isset($input['hosts'])) {
+                foreach($input['hosts'] as $index => $host) {
+                    $output['hosts'][$index] = array(
+                        'url' => esc_url_raw($host['url']),
+                        'username' => sanitize_user($host['username']),
+                        'password' => sanitize_text_field($host['password']),
+                        'strict_mode' => isset($host['strict_mode']) ? 1 : 0,
+                        'match_mode' => sanitize_text_field($host['match_mode']),
+                        'excluded_categories' => isset($host['excluded_categories']) 
+                            ? array_map('intval', $host['excluded_categories']) 
+                            : array(),
+                        'roles' => isset($host['roles']) 
+                            ? array_map('sanitize_text_field', $host['roles']) 
+                            : array()
+                    );
                 }
             }
-            return $all_types;
+            
+            return $output;
         }
-       
+
+        // ============ [ MÉTODOS DE RENDERIZAÇÃO ] ============ //
+
+        private function spsv2_render_host_fields($index, $host) {
+            ?>
+            <div class="spsv2-host-section">
+                <h3><?php _e('Configurações do Site', 'spsv2_text_domain'); ?></h3>
+                
+                <table class="form-table">
+                    <!-- Campos existentes -->
+                    <tr>
+                        <th><?php _e('URL do Site', 'spsv2_text_domain'); ?></th>
+                        <td>
+                            <input type="url" name="spsv2_settings[hosts][<?php echo $index; ?>][url]" 
+                                   value="<?php echo esc_url($host['url']); ?>" 
+                                   class="regular-text" required>
+                        </td>
+                    </tr>
+                    
+                    <!-- Novo Campo: Categorias Excluídas -->
+                    <tr>
+                        <th><?php _e('Categorias Excluídas', 'spsv2_text_domain'); ?></th>
+                        <td>
+                            <?php $this->spsv2_render_category_checkboxes($index, $host); ?>
+                        </td>
+                    </tr>
+                    
+                    <!-- Demais campos (usuário, senha, modos) -->
+                    <?php $this->spsv2_render_common_fields($index, $host); ?>
+                </table>
+                
+                <button type="button" class="button spsv2-remove-host">
+                    <?php _e('Remover Site', 'spsv2_text_domain'); ?>
+                </button>
+                <hr>
+            </div>
+            <?php
+        }
+
+        private function spsv2_render_category_checkboxes($index, $host) {
+            $categories = get_categories(array('hide_empty' => false));
+            $excluded = $host['excluded_categories'] ?? array();
+            
+            foreach($categories as $cat) {
+                $checked = in_array($cat->term_id, $excluded) ? 'checked' : '';
+                echo '<label>';
+                echo '<input type="checkbox" name="spsv2_settings[hosts]['.$index.'][excluded_categories][]" 
+                      value="'.esc_attr($cat->term_id).'" '.$checked.'>';
+                echo esc_html($cat->name);
+                echo '</label><br>';
+            }
+        }
+
+        // ============ [ MÉTODOS UTILITÁRIOS ] ============ //
+
+        public function spsv2_get_settings() {
+            $defaults = array(
+                'hosts' => array(
+                    array(
+                        'url' => '',
+                        'username' => '',
+                        'password' => '',
+                        'strict_mode' => 1,
+                        'match_mode' => 'title',
+                        'excluded_categories' => array(),
+                        'roles' => array('administrator')
+                    )
+                )
+            );
+            
+            return wp_parse_args(get_option('spsv2_settings'), $defaults);
+        }
+
+        public function spsv2_get_hosts() {
+            $settings = $this->spsv2_get_settings();
+            return $settings['hosts'] ?? array();
+        }
     }
 
-    global $sps_settings;
-    $sps_settings = new SPS_Settings();
-
+    global $spsv2_settings;
+    $spsv2_settings = new SPSv2_Settings();
 }
-
-?>
